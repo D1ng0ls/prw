4071623d9e1533364d268dbe4cc27a612cd36e26
D1ng0ls <4b4c4t3666@gmail.com>
2022-03-23 11:41:26 -0300

    exercios em aula

---
+++ aula5/ex2.php
@@ -0,0 +1,10 @@
+<?php
+    $n1 = 7;
+    $n2 = 8;
+    $media = ($n1 + $n2)/2;
+    if($media >= 7){
+        echo "Você foi aprovado(a)!";
+    } else {
+        echo "Você foi reprovado(a)!";
+    }
+?>
